07cc5dc04fdfd69b46c161840f478365a58f53af
Alp <aalpkilicc@gmail.com>
2023-12-10 20:05:26 +0300

    Add files via upload

---
+++ webapp/controller/App.controller.js
@@ -0,0 +1,60 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller",
+    "sap/m/library",
+    "sap/ui/core/Locale",
+    "sap/ui/core/LocaleData",
+    "sap/ui/model/type/Currency",
+    "sap/m/ObjectAttribute",
+], (Controller, mobileLibrary, Locale, LocaleData, Currency, ObjectAttribute, ) => {
+    "use strict";
+
+    return Controller.extend("databinding.controller.App", {
+        formatMail(sFirstName, sLastName) {
+            var oBundle = this.getView().getModel("i18n").getResourceBundle();
+            return mobileLibrary.URLHelper.normalizeEmail(
+                sFirstName + "." + sLastName + "@example.com",
+                oBundle.getText("mailSubject", [sFirstName]),
+                oBundle.getText("mailBody"));
+        },
+
+        formatStockValue(fUnitPrice, iStockLevel, sCurrCode) {
+            var sBrowserLocale = sap.ui.getCore().getConfiguration().getLanguage();
+            var oLocale = new Locale(sBrowserLocale);
+            var oLocaleData = new LocaleData(oLocale);
+            var oCurrency = new Currency(oLocaleData.mData.currencyFormat);
+            return oCurrency.formatValue([fUnitPrice * iStockLevel, sCurrCode], "string");
+        },
+
+        onItemSelected(oEvent) {
+            var oSelectedItem = oEvent.getSource();
+            var oContext = oSelectedItem.getBindingContext("products");
+            var sPath = oContext.getPath();
+            var oProductDetailPanel = this.byId("productDetailsPanel");
+            oProductDetailPanel.bindElement({ path: sPath, model: "products" });
+        },
+
+        productListFactory(sId, oContext) {
+            var oUIControl;
+
+            // Decide based on the data which dependent to clone
+            if (oContext.getProperty("UnitsInStock") === 0 && oContext.getProperty("Discontinued")) {
+                // The item is discontinued, so use a StandardListItem
+                oUIControl = this.byId("productSimple").clone(sId);
+            } else {
+                // The item is available, so we will create an ObjectListItem
+                oUIControl = this.byId("productExtended").clone(sId);
+
+                // The item is temporarily out of stock, so we will add a status
+                if (oContext.getProperty("UnitsInStock") < 1) {
+                    oUIControl.addAttribute(new ObjectAttribute({
+                        text: {
+                            path: "i18n>outOfStock"
+                        }
+                    }));
+                }
+            }
+
+            return oUIControl;
+        },
+    });
+});
